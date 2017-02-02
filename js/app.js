c096a2cd483819f3779d0c30720494a526a6b40d
Sebastiano <sebastianolaini@gmail.com>
2017-02-02 20:47:55 +0100

    Add button

--- js/app.js
+++ js/app.js
@@ -8,8 +8,8 @@ var city;
   $.getJSON("http://ip-api.com/json",function(data2){
     lat = data2.lat;
     long = data2.lon;
-    city = data2.city
-    console.log(data2)
+    city2 = data2.city
+//    console.log(data2)
 
 //var url = "http://api.openweathermap.org/data/2.5/weather?lat=";
 var url = "http://api.openweathermap.org/data/2.5/weather?q=";
@@ -17,20 +17,39 @@ var appid = "&APPID=a4b3eb234adddc9781ee965ab3aba708";
   
   //AJAX request weather API
 //  $.getJSON( url + lat + "&lon=" + long + appid, function(data) {
-   $.getJSON( url + city + appid, function(data) {
+   $.getJSON( url + city2 + appid, function(data) {
 
   var city = data.name;
   var condition = data.weather[0].description;
   var temperature = data.main.temp;
+  var temperature_c = (temperature - 273).toFixed(2); 
+  var temperature_f = (((temperature - 273) * 9 / 5) + 32).toFixed(2); 
 
   $("#city").html(city);
-  $("#temperature").html(temperature);
+  $("#temperature_f").html(temperature_f + " °F");
+  $("#temperature_c").html(temperature_c + " °C");
   $("#condition").html(condition);
 
-  console.log (data);
+
+//  console.log (data);
   }); //end JSON weather API
 
   }); //end JSON location
+
+  $(".change_unit").click(function() {
+  if ($("#temperature_f").hasClass("hidden")) {
+    $("#temperature_c").addClass("hidden");
+    $("#temperature_f").removeClass("hidden");
+    $(".change_unit").html("°F to °C");
+} else {
+      $("#temperature_f").addClass("hidden");
+    $("#temperature_c").removeClass("hidden");
+    $(".change_unit").html("°C to °F");
+  }
+}); //end change unit click
+
+
+
 
 }); //end ready
 
